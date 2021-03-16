126b56191c984667e517f5032ed868dc68efbd73
Janos <romanjanos0504@gmail.com>
2021-03-16 18:34:06 +0100

    create, update

--- src/app/page/user-editor/user-editor.component.ts
+++ src/app/page/user-editor/user-editor.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import { User } from 'src/app/model/user';
@@ -32,9 +32,27 @@ export class UserEditorComponent implements OnInit {
   constructor(
     private userService: UserService,
     private activatedRoute: ActivatedRoute,
+    private router: Router,
   ) { }
 
   ngOnInit(): void {
+  }
+
+  onFormSubmit(form: NgForm, element: User): void {
+    try {
+      if (element.id == 0) {
+        this.userService.create(element).subscribe(
+          () => this.router.navigate(['/'])
+        );
+      }
+      else {
+        this.userService.update(element).subscribe(
+          () => this.router.navigate(['/'])
+        );
+      }
+    } catch (error) {
+      
+    }
   }
 
 }
